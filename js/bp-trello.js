77cd60cfa51d252840f85f5e073e33eb4a17ab3d
Cycododge <cycododge@server.fake>
2013-08-02 08:28:08 -0700

    Re-written using Trello object data

---
+++ js/bp-trello.js
@@ -0,0 +1,172 @@
+/*
+AUTHOR
+	Cycododge
+
+UPDATED
+	7/31/2013
+*/
+
+(function($){
+	/* "GLOBAL" VARS */
+	var pData = { //the current board status with demo structure
+			totalCards:0,
+			totalComplete:0
+		},
+		set = { //default settings
+			progressOfCards:true,
+			progressOfScrum:false,
+			countCheckLists:true,
+			rememberGlobally:false,
+			lastSelectedList:'',
+			refreshTime:30000
+		},
+		percentageComplete = 0,
+		backupKeywords = ['{bp-done}','done','complete','finished','closed'], //in order of priority
+		lastDoneList = []; //contains current drop list data to compare against
+
+
+	/* EVENTS */
+
+	//check that the UI still exists
+		//and load the data continuously (can't figure out how to inject working listeners!)
+	injectUI(); //initial call
+	setInterval(injectUI,set.refreshTime);
+
+	//reload when the done list setting is changed
+	$('body').on('change','.bp-doneList select',function(){
+		//save the newly selected list
+		set.lastSelectedList = $(this).find('option:selected').val();
+
+		//update the progress
+		loadData();
+	});
+
+
+	/* FUNCTIONS */
+
+	//push the progress bar to the UI if it doesn't exist
+	function injectUI(){
+		//if the UI doesn't exist
+		if(!$('.ext-bp').length){
+			// console.log('injectUI()');
+
+			//add it to the page
+			$('#board-header').after('<div class="ext-bp"><div class="bp-barContainer"><div class="bp-progress"><span class="bp-pc">0</span>%</div></div><div class="bp-doneList"><select></select></div></div>');
+
+			//show the progress bar
+			$('.ext-bp').slideDown();
+		}
+
+		//reload the data
+		loadData();
+	}
+
+	//update the list of cards
+	function updateDoneOptions(_lists){
+		var nextDoneList = [], listOptions = [];
+
+		//loop through the current lists on the board
+		for(var listID in _lists){
+			if(!_lists.hasOwnProperty(listID)){ continue; }
+			if(_lists[listID].attributes.closed){ continue; } //skip if the list is closed
+
+			//add to new array
+			nextDoneList.push({id:listID,title:_lists[listID].attributes.name});
+		}
+
+		//if a selected list hasn't been specified
+		if(!set.lastSelectedList){
+			//loop through list of keywords to check titles against
+			for(var i = 0, ii = backupKeywords.length; i < ii; i++){
+				//loop through each list title
+				for(var x = 0, xx = nextDoneList.length; x < xx; x++){
+					//if this keyword exists in this list title
+					if(nextDoneList[x].title.toLowerCase().indexOf(backupKeywords[i].toLowerCase()) >= 0){
+						set.lastSelectedList = nextDoneList[x].id; //set this list as selected
+						ii = xx = 0; //selection found, break out of loops
+					}
+				}
+			}
+		}
+
+		//compare the nextDoneList with lastDoneList
+		if(JSON.stringify(nextDoneList) != JSON.stringify(lastDoneList)){
+			//update lastDoneList with nextDoneList
+			lastDoneList = JSON.parse(JSON.stringify(nextDoneList));
+
+			//loop through nextDoneList
+			for(var i = 0, ii = nextDoneList.length; i < ii; i++){
+				//create the option lists AND set selected
+				listOptions.push('<option value="'+nextDoneList[i].id+'"'+(nextDoneList[i].id == set.lastSelectedList ? ' selected':'')+'>'+nextDoneList[i].title+'</option>');
+			}
+
+			//output the list to the page
+			$('.ext-bp .bp-doneList select').html(listOptions.join(''));
+		}
+	}
+
+	//refresh the data from the board
+	function loadData(){
+		//reset
+		pData.totalCards = 0;
+		pData.totalComplete = 0;
+		_lists = ModelCache._cache.List;
+		_cards = ModelCache._cache.Card;
+
+		//update the drop down
+		updateDoneOptions(_lists);
+
+		//for each list
+		for(var listID in _lists){
+			if(!_lists.hasOwnProperty(listID)){ continue; } //skip if not a list
+			if(_lists[listID].attributes.closed){ continue; } //skip if the list is closed
+
+			//loop through each card
+			for(var cardID in _cards){
+				if(!_cards.hasOwnProperty(cardID)){ continue; } //skip if not a card
+				if(_cards[cardID].attributes.closed){ continue; } //skip if the card is closed
+				if(_cards[cardID].attributes.idList != listID){ continue; } //skip if the card doesn't belong to this list
+
+				//debug
+				if(_cards[cardID].checklistList.length){
+					console.log(_cards[cardID].checklistList);
+				}
+
+				//if allowed count checklists for this card
+				var numCheckLists = 0;
+				if(set.countCheckLists){
+					//loops through available checklists
+					for(var i = 0, ii = _cards[cardID].checklistList.length; i < ii; i++){
+						//count the number of checklist items towards total
+						numCheckLists += _cards[cardID].checklistList.models[i].attributes.checkItems.length;
+					}
+				}
+
+				pData.totalCards += numCheckLists || 1; //add this card to the total
+
+				//if this card is in the "done" list, count towards complete
+				if(index == $('.ext-bp .bp-doneList option:selected').val()){
+					pData.totalComplete += numCheckLists || 1; //add this card to the total
+				}
+			}
+		}
+
+		//update the progress on the board
+		updateProgress();
+	}
+
+	//update the progress bar
+	function updateProgress(){
+		//determine percentage
+		var newPercent = Math.round((pData.totalComplete / pData.totalCards) * 100);
+
+		//don't update if nothing changed
+		if(percentageComplete == newPercent){ return; }
+
+		//update the global var
+		percentageComplete = newPercent;
+		//adjust the progress bar
+		$('.bp-progress').width(percentageComplete+'%').find('.bp-pc').text(percentageComplete);
+		// console.log('updateProgress()');
+	}
+})(jQuery);
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -3,180 +3,24 @@ AUTHOR
 	Cycododge
 
 UPDATED
-	7/28/2013 v1.0.0
+	7/31/2013 v1.0.0
+
+PURPOSE
+	Injects script into DOM for access to page variables
 */
-
-/* "GLOBAL" VARS */
-pData = { //the current board status with demo structure
-	totalCards:0,
-	totalComplete:0
-},
-set = { //default settings
-	progressOfCards:true,
-	progressOfScrum:false,
-	countCheckLists:true,
-	rememberGlobally:false,
-	lastSelectedList:''
-},
-percentageComplete = 0,
-backupDoneTitles = ['{bp-done}','done','complete','finished','closed']; //in order of priority
-
-
-/* EVENTS */
-function initEvents(){
-	//check that the UI still exists
-		//and load the data continuously (can't figure out how to inject working listeners)
-	setInterval(injectUI,1000);
-
-	//reload when the done list setting is changed
-	$('body').on('change','.bp-doneList select',function(){
-		//save the newly selected list
-		set.lastSelectedList = $('.ext-bp .bp-doneList option:selected').text();
-
-		//update the progress
-		loadData();
-	});
-}
-
-
-/* FUNCTIONS */
-
-//push the progress bar to the UI if it doesn't exist
-function injectUI(){
-	//if the UI doesn't exist
-	if(!$('.ext-bp').length){
-		console.log('injectUI()');
-
-		//add it to the page
-		$('#board-header').after('<div class="ext-bp"><div class="bp-barContainer"><div class="bp-progress"><span class="bp-pc">0</span>%</div></div><div class="bp-doneList"><select></select></div></div>');
-
-		//show the progress bar
-		$('.ext-bp').slideDown();
-
-	}
-
-	//reload the data
-	loadData();
-}
-
-//update the list of cards
-function updateDoneOptions(){
-	console.log('updateDoneOptions()');
-	var newSelection = '',
-		$lists = $('.list');
-
-	//decide which list is selected
-		//if not set by user
-	if(!set.lastSelectedList){
-		//loop through list of titles to check against
-		for(var i = 0, ii = backupDoneTitles.length; i < ii; i++){
-			//exit the loop if this is now set
-			if(newSelection){ break; }
-
-			//check each list and make best guess
-			$.each($lists,function(index){
-				var $this = $(this),
-					listTitle = $this.find('.list-title h2').text();
-
-				//check if this list matches
-				if(listTitle.toLowerCase().indexOf(backupDoneTitles[i].toLowerCase()) >= 0){
-					newSelection = listTitle; //set this list as selected
-					return; //stop checking list when match found
-				}
-			});
-		}
-
-		//save the new selection as the last selected
-		set.lastSelectedList = newSelection;
-	}else{
-		newSelection = set.lastSelectedList;
-	}
-
-	//build "done" list options
-	$.each($lists,function(index){
-		var $this = $(this),
-			listTitle = $this.find('.list-title h2').text();
-
-		//skip this list if it has no title
-		if(!listTitle){ return; }
-
-		//see if this element is already in the list
-		if(!$('select option:contains("'+listTitle+'")').length){
-			//add this list, and possibly make it selected
-			var selected = (listTitle == newSelection ? ' selected':'');
-			$('.ext-bp .bp-doneList select').append('<option value="'+index+'"'+selected+'>'+listTitle+'</option>');
-		}
-	});
-}
-
-//refresh the data from the board
-function loadData(){
-	console.log('loadData()');
-	//reset
-	pData.totalCards = 0;
-	pData.totalComplete = 0;
-
-	//update the drop down
-	updateDoneOptions();
-
-	//for each list
-	$.each($('.list'),function(index){
-		var $this = $(this);
-
-		//loop through each card
-		$.each($this.find('.list-card').not('.hide'),function(){
-			var numCheckLists = 0;
-
-			//exit if this card isn't created yet
-			if($(this).hasClass('js-composer')){ return; }
-
-			//if allowed, count checklists for this card
-			if(set.countCheckLists){
-				//look for a checklist
-				var findChecklist = $(this).find('.badges .icon-checklist').parent().find('.badge-text');
-
-				//if there is a checklist
-				if(findChecklist.length){
-					numCheckLists += parseInt(findChecklist.text().split('/')[1],10);
-				}
-			}
-
-			pData.totalCards += numCheckLists || 1; //add this card to the total
-
-			//if this card is in the "done" list, count towards complete
-			if(index == $('.ext-bp .bp-doneList option:selected').val()){
-				pData.totalComplete += numCheckLists || 1; //add this card to the total
-			}
-		});
-	});
-
-	//update the progress on the board
-	updateProgress();
-}
-
-//update the progress bar
-function updateProgress(){
-	//determine percentage
-	var newPercent = Math.round((pData.totalComplete / pData.totalCards) * 100);
-
-	//don't update if nothing changed
-	if(percentageComplete == newPercent){ return; }
-
-	//update the global var
-	percentageComplete = newPercent;
-	//adjust the progress bar
-	$('.bp-progress').width(percentageComplete+'%').find('.bp-pc').text(percentageComplete);
-	console.log('updateProgress()');
-}
-
 
 /* IMMEDIATE */
 //intializes the script when the page is ready
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
-		initEvents();
-	}
+		if (document.readyState === "complete") {
+			clearInterval(readyStateCheckInterval);
+
+			//add the script to the page
+			var scr = document.createElement("script");
+			scr.type = "text/javascript";
+			scr.src = chrome.extension.getURL("js/bp-trello.js")+'?v='+chrome.app.getDetails().version;
+			(document.head || document.body || document.documentElement).appendChild(scr);
+		}
 	}, 10);
 });
