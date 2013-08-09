6027783f503d8628c381f8dacae56e33ab9071de
Cycododge <cycododge@server.fake>
2013-08-08 18:40:46 -0700

    clean up and analytics

---
+++ js/analytics.js
@@ -0,0 +1,19 @@
+/*
+AUTHOR
+	Cycododge
+
+UPDATED
+	8/8/2013
+
+PURPOSE
+	Let's Google Analytics know about installation.
+*/
+
+//setup
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-596722-38', 'progress-for-trello.com');
+ga('send', 'pageview');
